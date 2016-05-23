7bf2baa395e69f23876c80d78341bcdb243c08dc
Christian Reuter <christian@thoughtbot.com>
2016-05-23 16:05:17 -0400

    Add segment for analytics

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,6 +19,5 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-//
 import editor from "./editor"
 import gravatar from "./gravatar"
